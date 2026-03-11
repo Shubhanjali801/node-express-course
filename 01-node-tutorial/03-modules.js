fdd3cd2d3056b388e96fe16aa5f260dde243e815
Shubhanjali801 <shubhanjali32@gmail.com>
2026-03-11 16:34:41 +0530

    refactor: reorganize console logs and improve code readability

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,11 +1,13 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./04-names')
-console.log(names)
 
+const names = require('./04-names')
 const sayHi = require('./05-utils')
 const data = require('./06-alternative-flavor')
+
 require('./07-mind-grenade')
+console.log(data)
+console.log(names)
 
 sayHi('susan')
 sayHi(names.john)
