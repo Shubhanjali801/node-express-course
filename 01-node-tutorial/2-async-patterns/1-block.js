b9841c47f88b6007863e042b1a9336f885a679c5
Shubhanjali801 <shubhanjali32@gmail.com>
2026-03-11 16:54:37 +0530

    fix: reduce loop iterations in blocking code for improved performance

--- 01-node-tutorial/2-async-patterns/1-block.js
+++ 01-node-tutorial/2-async-patterns/1-block.js
@@ -6,8 +6,8 @@ const server = http.createServer((req, res) => {
   }
   if (req.url === '/about') {
     // blocking code
-    for (let i = 0; i < 1000; i++) {
-      for (let j = 0; j < 1000; j++) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
         console.log(`${i} ${j}`)
       }
     }
